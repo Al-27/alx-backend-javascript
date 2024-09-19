f74207c8e82bfc5301b066d07c33d68783bffd48
Al-27 <125827852+Al-27@users.noreply.github.com>
2024-09-19 11:25:45 +0000

    eslint

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,13 +1,14 @@
 const readline = require('node:readline');
 
 const read = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
+  input: process.stdin,
+  output: process.stdout,
+
 });
 
-console.log("Welcome to Holberton School, what is your name?");
+console.log('Welcome to Holberton School, what is your name?');
 
-read.question("Your name is: ", () => {
-    console.log("This important software is now closing");
-    read.close();
+read.question('Your name is: ', () => {
+  console.log('This important software is now closing');
+  read.close();
 });
