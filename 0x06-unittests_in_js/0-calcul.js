0764b78356ee351ce83a9fa875f680ccfb5c46ae
Al-27 <125827852+Al-27@users.noreply.github.com>
2024-09-22 13:59:46 +0000

    0x06

---
+++ 0x06-unittests_in_js/0-calcul.js
@@ -0,0 +1,4 @@
+exports.calculateNumber = (a,b) => {
+    return Math.round(a) + Math.round(b);    
+}
+ 
---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,21 @@
+const calc = require('./0-calcul').calculateNumber;
+const assert = require('node:assert');
+
+let tests = [
+    assert.equal(calc(2,3),5),
+    assert.equal(calc(2.1,3),5),
+    assert.equal(calc(2.4,3.3),5),
+    assert.equal(calc(2.4,3.4),5),
+    assert.equal(calc(2.32,3.3),5),
+    assert.equal(calc(2.8,3.4),6),
+    assert.equal(calc(2.9,3.4),6),
+    assert.equal(calc(2.4,3.9),6),
+    assert.equal(calc(2.8,3.8),7),
+    assert.equal(calc(2.5,3.7),7)
+];
+
+describe('calculateNumber', ()=>{
+    tests.forEach(el => it('...',  ()=>{
+        el;
+    }));
+});
